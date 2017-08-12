13e4aed9101bc58b1276771b618ff3db018eccae
Slidejiveman <rdnotlaw91@outlook.com>
2017-08-12 10:50:07 -0500

    Changed the date converter to get rid of some warning I was getting.

--- src/eventplannerDAO/converter/LocalDateConverter.java
+++ src/eventplannerDAO/converter/LocalDateConverter.java
@@ -1,10 +1,7 @@
 package eventplannerDAO.converter;
 
 import java.sql.Date;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
@@ -20,16 +17,13 @@ import javax.persistence.Converter;
 @Converter(autoApply = true)
 public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
 
-	  @Override
-	  public Date convertToDatabaseColumn(LocalDate date) {
-	      Instant instant = 
-			date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
-	      return (Date)Date.from(instant);
-	  }
+	@Override
+    public Date convertToDatabaseColumn(LocalDate locDate) {
+      return (locDate == null ? null : Date.valueOf(locDate));
+    }
 
-	  @Override
-	  public LocalDate convertToEntityAttribute(Date date) {
-	      Instant instant = Instant.ofEpochMilli(date.getTime());
-	      return LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalDate();
-	  }
+    @Override
+    public LocalDate convertToEntityAttribute(Date sqlDate) {
+      return (sqlDate == null ? null : sqlDate.toLocalDate());
+    }
 }
